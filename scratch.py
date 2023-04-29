47727a0f6267278b26cca7c65aef65fd9c62c8c9
Michael Macfarlan <mdmacfar@gmail.com>
2023-04-29 19:19:10 -0400

    updates and demo scen

--- scratch.py
+++ scratch.py
@@ -1,4 +1,3 @@
-import mcs_pkg
 from mcs_pkg import *
 
 # create a sample game with two dice
@@ -8,3 +7,12 @@ game = Game([die1, die2])
 
 # play the game 1000 times
 game.play(1000)
+
+# show the jackpot results
+an = Analyzer(game)
+an_jackpot = an.count_jackpots()
+an_flips = game.results.shape[0]
+an_jackpot
+an_flips
+an_freq = an_jackpot / an_flips
+print(an_freq)
